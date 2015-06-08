9a567cef8edbb571823970379f71cd6b0f9458d7
manu <manu_bulboaca@yahoo.com>
2015-06-08 18:32:44 +0300

    ui changes

--- public/AngularApp/src/js/homeController.js
+++ public/AngularApp/src/js/homeController.js
@@ -7,12 +7,18 @@ app.controller('homeController', function($rootScope, $scope, $http, $location, 
 
 	$scope.users = [];
 	serviceHttp.get("/webService/usersT", function(json) {
-		console.log(json.length);
+		//console.log(json.length);
 		for (var i = 0; i < json.length; i++) {
-			console.log(json[i]);
+			//console.log(json[i]);
 			$scope.users.push(json[i]);
-			console.log("USER " + $scope.users[i].name)
+			//console.log("USER " + $scope.users[i].name)
 		}
+		
+		// for (var i = 0; i < 1000	; i++) {
+			// $scope.users.push(
+				// {"user_id": "user "+i, "name": "user de "+ i, "email": "email"+i, "address": "adresa"+i, "idate": "sysdate"}
+				// );
+		// }
 	});
 
 	$scope.selectUser = function(item) {
